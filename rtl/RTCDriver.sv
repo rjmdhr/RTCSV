1a11e2fbe1df7c02cc9e4a74c9f09c78c0aba68b
Raj Mudhar <rmudhar@outlook.com>
2021-06-15 21:53:27 -0400

    ignore backups

--- rtl/RTCDriver.sv
+++ rtl/RTCDriver.sv
@@ -1,12 +1,18 @@
 'timescale 1ms/1us
 
 module RTCDriver(
-	input logic clock36768Hz,
-	output logic [5:0] seven_seg[7:0] 
+	input logic clock36768Hz, //crystal oscillator frequency
+	output logic [5:0] seven_seg[7:0] //6 displays (2Hr,2Min,2Sec)
 );
 
 logic resetn;
 
-parameter clock1Hz_div = 18384;
+// frequency division 
+parameter clock1Hz_div_count = 18383;
+logic [14:0] clock1Hz_div_count;
+logic clock1Hzbuf;
+logic clock1Hz;
+
+
 
 end module 
