117216c27608ea28704606d5778c9cf7b0e110d9
Raj Mudhar <rmudhar@outlook.com>
2021-06-17 17:28:48 -0400

    Add counter enable and counter prototype

--- rtl/RTCDriver.sv
+++ rtl/RTCDriver.sv
@@ -6,34 +6,40 @@
 // internal clock
 module RTCDriver(
 	input logic clock50MHz, //50 MHz clock
-	input logic [3:0] push_button, //increase digit
-	input logic switch, //manual mode
+	input logic [2:0] push_button, //increase digit
+	input logic reset_button, // master reset
+	input logic man_switch, //manual clock set
 	output logic [5:0] seven_seg[7:0] //6 displays, arranged in pin format 
 	// 10-9-1-2-4-6-7 | g-f-e-d-c-b-a.
+
+	// total input|outputs = 6|48
 );
 
 logic resetn;
 
 // Divide the frequency to 1Hz through a buffer and edge dete
-parameter 1kHz_count = 24999; //50E6/25E3 = 2000 ~/sec
-parameter 1Hz_count = 24999999; //50E6/25E6 = 2 ~/sec
+parameter 1kHz_count = 24999; //50E6/25E3 = 2000x ~/sec
+parameter 1Hz_count = 24999999; //50E6/25E6 = 2x ~/sec
 
 logic [15:0] clock1kHz_div_count;
 logic [24:0] clock1Hz_div_count;
 logic clock1Hzbuf;
 logic clock1Hz;
+logic count_en;
 
 logic [5:0] sec_counter;
 logic [5:0] min_counter;
 logic [3:0] hour_counter;
-logic count_en; //1ns pulse
 
 logic [6:0] 7_seg_HH, 7_seg_MM, 7_seg_ss;
 
-// divide the clock down to 1Hz
-always_ff @(posedge clock50MHz or negedge resetn) begin : clock_divider
+assign resetn = reset_button;
+
+/** clock divider */
+// add counter to reset clock every 0.5s
+always_ff @(posedge clock50MHz or negedge resetn) begin : clock_divider_1Hz
 	if (resetn == 1'b0) begin
-		clock1Hz_div_count <= 0;
+		clock1Hz_div_count <= 1'b0;
 	end
 	else begin
 		if (clock1Hz_div_count < 1Hz_count) begin
@@ -41,13 +47,57 @@ always_ff @(posedge clock50MHz or negedge resetn) begin : clock_divider
 		end
 		else begin
 			clock1Hz_div_count <= 16'd0;
+		end
+	end
+end
+// invert clock every 0.5s
+always_ff @(posedge clock50MHz or negedge resetn) begin : clock_invert_1Hz
+	if (resetn == 1'b0) begin
+		clock1Hz <= 1'b0
+	end
+	else begin
+		if (clock1Hz_div_count <= 23'd0) begin
 			clock1Hz <= ~clock1Hz;
+		end
+	end
+end
+// buffer the 1Hz clock for use as a 20ns "pulse"
+always_ff @(posedge clock50MHz or negedge resetn) begin : clock_buf_1Hz
+	if (resetn == 1'b0) begin
+		clock1Hzbuf <= 1'b0;
+	end
+	else begin
+		clock1Hzbuf <= clock1Hz;
+	end
+end
+// generate the 20ns pulse for the counter
+always_comb begin : counter_enable
+	count_en = clock1Hz & ~clock1Hzbuf;
+end
+
+/** HH:MM:SS counters */
+always_ff @(posedge clock50MHz or negedge resetn) begin : seconds_counter
+	if (resetn == 1'b0) begin
+		
+	end
+	else begin
+		if (~man_switch && count_en) begin
+			sec_counter <= sec_counter + 6'd1;
 		end
 	end
 end
 
 
-	
+/**
+// 1kHz clock divider (shift register button debouncing)
+always_ff @(posedge clock50MHz or negedge resetn) begin : clock_divider_1kHz
+	if (resetn == 1'b0) begin
+		clock1kHz_div_count <= 16'd0
+	end
+end
+*/
+
+
 
 
 end module 
