7460e8fd386379ae2dc1e792d1777f1dec070d78
Raj Mudhar <rmudhar@outlook.com>
2021-06-19 18:03:15 -0400

    Create project and test bench

--- rtcsv.sv
+++ rtcsv.sv
@@ -1,15 +1,18 @@
-'timescale 1ns/100ps
+`timescale 1ns/100ps
 
 // This module takes a reference clock and drives 6 7-segment displays to display
 // the time in HH:MM:SS format. It also contains a toggle switch for a time set mode
 // and 3 push buttons to set the time columns. The accuracy will be limited to the
 // internal clock
+// DE2-115 SOC board
+// note: switches are default logic 0 when in "down position"
+//       buttons are active low (logic high when not pressed)
 module RTCDriver(
 	input logic clock50MHz, //50 MHz clock
 	input logic [2:0] push_button, //increase digit
 	input logic reset_button, // master reset
 	input logic man_switch, //manual clock set
-	output logic [5:0] seven_seg[7:0] //6 displays, arranged in pin format 
+	output logic [5:0] seven_seg[7:0] //6 displays, arranged in pin format
 	// 10-9-1-2-4-6-7 | g-f-e-d-c-b-a.
 
 	// total input|outputs = 6|48
@@ -17,21 +20,26 @@ module RTCDriver(
 
 logic resetn;
 
-// Divide the frequency to 1Hz through a buffer and edge dete
-parameter 1kHz_count = 24999; //50E6/25E3 = 2000x ~/sec
-parameter 1Hz_count = 24999999; //50E6/25E6 = 2x ~/sec
+// Divide the frequency to 1Hz through a buffer and edge detection
+parameter max_1kHz_cnt = 24999; //50E6/25E3 = 2000x ~/sec
+parameter max_1Hz_cnt = 24999999; //50E6/25E6 = 2x ~/sec
 
 logic [15:0] clock1kHz_div_count;
 logic [24:0] clock1Hz_div_count;
 logic clock1Hzbuf;
 logic clock1Hz;
-logic count_en;
+logic count_en; //20 ns pulse for counter
 
-logic [5:0] sec_counter;
-logic [5:0] min_counter;
-logic [3:0] hour_counter;
+logic [3:0] sec_ctr_0; //1s column of seconds
+logic [3:0] sec_ctr_1; //10s column of seconds
+logic [3:0] min_ctr_0; //1s column of minutes
+logic [3:0] min_ctr_1; //10s column of minutes
+logic [3:0] hr_ctr_0; //1s column of hours
+logic [3:0] hr_ctr_1; //10s column of hours
 
-logic [6:0] 7_seg_HH, 7_seg_MM, 7_seg_ss;
+//seven segment register pinouts
+//0 for 1s column, 1 for 10s column
+logic [6:0] sevsegHH0, sevsegHH1, sevsegMM0, sevsegMM1, sevsegSS0, sevsegSS1;
 
 assign resetn = reset_button;
 
@@ -42,7 +50,7 @@ always_ff @(posedge clock50MHz or negedge resetn) begin : clock_divider_1Hz
 		clock1Hz_div_count <= 1'b0;
 	end
 	else begin
-		if (clock1Hz_div_count < 1Hz_count) begin
+		if (clock1Hz_div_count < max_1Hz_cnt) begin
 			clock1Hz_div_count <= clock1Hz_div_count + 16'd1;
 		end
 		else begin
@@ -53,7 +61,7 @@ end
 // invert clock every 0.5s
 always_ff @(posedge clock50MHz or negedge resetn) begin : clock_invert_1Hz
 	if (resetn == 1'b0) begin
-		clock1Hz <= 1'b0
+		clock1Hz <= 1'b0;
 	end
 	else begin
 		if (clock1Hz_div_count <= 23'd0) begin
@@ -76,13 +84,15 @@ always_comb begin : counter_enable
 end
 
 /** HH:MM:SS counters */
+// seconds counter
 always_ff @(posedge clock50MHz or negedge resetn) begin : seconds_counter
 	if (resetn == 1'b0) begin
-		
+		sec_ctr_0 <= 4'd0;
+        sec_ctr_1 <= 4'd0;
 	end
 	else begin
-		if (~man_switch && count_en) begin
-			sec_counter <= sec_counter + 6'd1;
+		if (man_switch && count_en) begin
+            sec_ctr_0 <= sec_ctr_0 + 4'd1;
 		end
 	end
 end
@@ -97,7 +107,4 @@ always_ff @(posedge clock50MHz or negedge resetn) begin : clock_divider_1kHz
 end
 */
 
-
-
-
-end module 
+endmodule
